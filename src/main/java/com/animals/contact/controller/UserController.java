88c7e75e91d3cc92ffe2f178643ddd6d68cb42ff
Camille B <camille.bibaut0@gmail.com>
2023-02-08 17:02:08 +0100

    Fix sign-up and redirect after sign-up

--- src/main/java/com/animals/contact/controller/UserController.java
+++ src/main/java/com/animals/contact/controller/UserController.java
@@ -3,12 +3,14 @@ package com.animals.contact.controller;
 import com.animals.contact.entity.User;
 import com.animals.contact.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Controller
 public class UserController {
 
     @Autowired
@@ -23,6 +25,6 @@ public class UserController {
     @PostMapping("/sign-up")
     public String addUser (User user) {
         userRepository.save(user);
-        return "redirect:home";
+        return "home.html";
     }
 }
