7fac1fc2bd7e911b6276fd62795bd3620d9782a6
Jat15 <jinan15@hotmail.com>
2023-02-09 23:44:22 +0100

    Create View Fragment
    - Add nav and nav-auth
    - Add footer

--- src/main/java/com/animals/contact/controller/MainController.java
+++ src/main/java/com/animals/contact/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.animals.contact.controller;
 
-import com.animals.contact.entity.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
