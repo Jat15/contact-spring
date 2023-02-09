598f5508e7498123473e65e6e878841b9c590c15
Jat15 <jinan15@hotmail.com>
2023-02-09 09:52:47 +0100

    Update  Controller Contact
    - Emulate first user

--- src/main/java/com/animals/contact/controller/ContactController.java
+++ src/main/java/com/animals/contact/controller/ContactController.java
@@ -1,25 +1,33 @@
 package com.animals.contact.controller;
 
-import com.animals.contact.entity.Contact;
-import com.animals.contact.repository.ContactRepository;
+import com.animals.contact.entity.User;
+import com.animals.contact.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping(path="/contact")
 public class ContactController {
 
+    private Long idEmulate = 1l;
+
     @Autowired
-    ContactRepository contactRepository;
+    UserRepository userRepository;
     @GetMapping(path="/list")
     public String getAllContact(Model model) {
-        Iterable<Contact> contacts = contactRepository.findAll();
-        model.addAttribute("contacts", contacts);
+        Optional<User> user = userRepository.findById(idEmulate);
 
-        return "list-contact";
+        if (user.isPresent()){
+            model.addAttribute("contacts", user.get().getContacts());
+            return "list-contact";
+        }
+
+        return "redirect:/home";
     }
 
 
