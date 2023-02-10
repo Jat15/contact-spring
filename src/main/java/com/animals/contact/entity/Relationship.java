31c1f23d4d40a3ddb8794d898619edb5e8e3b215
Jat15 <jinan15@hotmail.com>
2023-02-10 14:09:37 +0100

    Create Entity Embeddable RelationShipPK

--- src/main/java/com/animals/contact/entity/Relationship.java
+++ src/main/java/com/animals/contact/entity/Relationship.java
@@ -4,15 +4,62 @@ import jakarta.persistence.*;
 
 @Entity
 public class Relationship {
-    @OneToOne
-    private Tag tagSrc;
+    @EmbeddedId
+    private RelationShipPK id;
+
     @ManyToOne
-    @Id
     @JoinColumn(name = "contact_src")
+    @MapsId("contactSrcId")
     private Contact contactSrc;
 
     @ManyToOne
-    @Id
     @JoinColumn(name = "contact_dest")
+    @MapsId("contactDestId")
     private Contact contactDest;
+
+    @OneToOne
+    private Tag tagSrc;
+
+    public Relationship(RelationShipPK id, Contact contactSrc, Contact contactDest, Tag tagSrc) {
+        this.id = id;
+        this.contactSrc = contactSrc;
+        this.contactDest = contactDest;
+        this.tagSrc = tagSrc;
+    }
+
+    public Relationship() {
+
+    }
+
+    public RelationShipPK getId() {
+        return id;
+    }
+
+    public void setId(RelationShipPK id) {
+        this.id = id;
+    }
+
+    public Contact getContactSrc() {
+        return contactSrc;
+    }
+
+    public void setContactSrc(Contact contactSrc) {
+        this.contactSrc = contactSrc;
+    }
+
+    public Contact getContactDest() {
+        return contactDest;
+    }
+
+    public void setContactDest(Contact contactDest) {
+        this.contactDest = contactDest;
+    }
+
+    public Tag getTagSrc() {
+        return tagSrc;
+    }
+
+    public void setTagSrc(Tag tagSrc) {
+        this.tagSrc = tagSrc;
+    }
 }
