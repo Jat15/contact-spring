a650aadfbcea284d298d26e703d137b7afd06b26
Camille B <camille.bibaut0@gmail.com>
2023-02-10 15:55:15 +0100

    Fixing mini-profile errors

--- src/main/java/com/animals/contact/controller/ContactController.java
+++ src/main/java/com/animals/contact/controller/ContactController.java
@@ -4,6 +4,7 @@ import com.animals.contact.entity.Contact;
 import com.animals.contact.entity.User;
 import com.animals.contact.repository.ContactRepository;
 import com.animals.contact.repository.UserRepository;
+import com.animals.contact.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @Controller
@@ -24,10 +26,18 @@ public class ContactController {
     UserRepository userRepository;
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private ContactRepository contactRepository;
 
     @GetMapping(path="/list")
-    public String getAllContact(Model model) {
+    public String getAllContact(Model model, Principal principal) {
+        String userEmail = principal.getName();
+        User userProfile = userService.findUser(userEmail);
+
+        model.addAttribute("user", userProfile);
+
         Optional<User> user = userRepository.findById(idEmulate);
 
         if (user.isPresent()){
@@ -39,7 +49,12 @@ public class ContactController {
     }
 
     @GetMapping("/detail/{id}")
-    public String displayContactDetail(@PathVariable Long id, Model model){
+    public String displayContactDetail(@PathVariable Long id, Model model, Principal principal){
+        String userEmail = principal.getName();
+        User userProfile = userService.findUser(userEmail);
+
+        model.addAttribute("user", userProfile);
+
         Optional<Contact> contactOptional = contactRepository.findById(id);
         if(contactOptional.isPresent()){
             model.addAttribute("contact", contactOptional.get());
@@ -50,7 +65,12 @@ public class ContactController {
     }
 
     @GetMapping("/add")
-    public String displayFormAddContact(Model model){
+    public String displayFormAddContact(Model model, Principal principal){
+        String userEmail = principal.getName();
+        User userProfile = userService.findUser(userEmail);
+
+        model.addAttribute("user", userProfile);
+
         model.addAttribute("contact", new Contact());
         return "add-contact";
     }
