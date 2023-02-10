97e3808c8c73378b5bc58730c47911d9d70fe766
Jat15 <jinan15@hotmail.com>
2023-02-10 23:49:42 +0100

    Update Repository User

--- src/main/java/com/animals/contact/repository/UserRepository.java
+++ src/main/java/com/animals/contact/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.animals.contact.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
     boolean existsUserByEmail(String email);
 }
