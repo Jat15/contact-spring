da1d73fd7fb59f498c04bcf06ed075075afc7836
Jat15 <jinan15@hotmail.com>
2023-02-12 20:10:05 +0100

    Create EntityAPI Img
    - For parse JSON

---
+++ src/main/java/com/animals/contact/entityApi/Img.java
@@ -0,0 +1,67 @@
+package com.animals.contact.entityApi;
+
+public class Img {
+    private int age;
+    private String date_added;
+    private String filename;
+    private String gender;
+    private String image_url;
+    private String last_served;
+    private String source;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDate_added() {
+        return date_added;
+    }
+
+    public void setDate_added(String date_added) {
+        this.date_added = date_added;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getLast_served() {
+        return last_served;
+    }
+
+    public void setLast_served(String last_served) {
+        this.last_served = last_served;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
